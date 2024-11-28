546c042c866bd3b3e698ff419952d3e384656316
sok0a <sokoa123@naver.com>
2024-11-28 15:29:10 +0900

    DTO 구현

--- src/main/java/org/example/bookmanagerapi/dto/BookRequestDto.java
+++ src/main/java/org/example/bookmanagerapi/dto/BookRequestDto.java
@@ -1,4 +1,15 @@
 package org.example.bookmanagerapi.dto;
 
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
 public class BookRequestDto {
+    private String title;
+    private String author;
+    private String isbn;
+    private LocalDate publishedDate;
 }
--- src/main/java/org/example/bookmanagerapi/dto/BookResponseDto.java
+++ src/main/java/org/example/bookmanagerapi/dto/BookResponseDto.java
@@ -1,4 +1,16 @@
 package org.example.bookmanagerapi.dto;
 
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
 public class BookResponseDto {
+    private Long id;
+    private String title;
+    private String author;
+    private String isbn;
+    private LocalDate publishedDate;
 }
