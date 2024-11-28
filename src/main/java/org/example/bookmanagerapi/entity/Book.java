8bb3e7f9c64dd54930aab3520eccbfb1426ce382
sok0a <sokoa123@naver.com>
2024-11-28 15:27:47 +0900

    Entity 구현

--- src/main/java/org/example/bookmanagerapi/entity/Book.java
+++ src/main/java/org/example/bookmanagerapi/entity/Book.java
@@ -1,4 +1,30 @@
 package org.example.bookmanagerapi.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String author;
+    private String isbn;
+    private LocalDate publishedDate;
+
+
 }
