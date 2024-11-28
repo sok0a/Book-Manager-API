76a8738b093cb2c8063d0bd81fc8b187e3ebfca5
sok0a <sokoa123@naver.com>
2024-11-28 15:19:37 +0900

    원격저장소 연결

---
+++ src/main/java/org/example/bookmanagerapi/BookManagerApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.bookmanagerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookManagerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookManagerApiApplication.class, args);
+    }
+
+}
